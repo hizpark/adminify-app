1f16618509b05e30dcc527d55ca079b2de520299
Harper Jang <harper.jang@outlook.com>
2025-04-22 00:49:53 +0800

    chore(lint): update ESLint and Prettier configurations

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,16 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintPluginPrettierRecommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +28,32 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+    },
+  },
+  {
+    files: ['**/*.{js,mjs}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+      sourceType: 'module',
+    },
+    extends: [eslintPluginPrettierRecommended],
+    rules: {
+      semi: ['error', 'never'],
+      'comma-dangle': ['error', 'always-multiline'],
+    },
+  },
+  {
+    files: ['**/*.cjs'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+      sourceType: 'script',
+    },
+    extends: [eslintPluginPrettierRecommended],
+    rules: {
+      semi: ['error', 'never'],
+      'comma-dangle': ['error', 'always-multiline'],
     },
   },
 )
